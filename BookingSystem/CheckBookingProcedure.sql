0987ca94688aefb9f53c366a8541178bbceb28ef
ephraimklestzick <effieklestz@yahoo.com>
2024-01-23 13:14:33 -0500

    Updated User

--- BookingSystem/CheckBookingProcedure.sql
+++ BookingSystem/CheckBookingProcedure.sql
@@ -1,10 +1,10 @@
 CREATE PROCEDURE 'CheckBooking'(booking_date DATE, table_number INT)
 BEGIN
-DECLARE bookedTable INT DEFAULT 0;
- SELECT COUNT(bookedTable)
-    INTO bookedTable
+DECLARE bookingTable INT DEFAULT 0;
+ SELECT COUNT(bookingTable)
+    INTO bookingTable
     FROM Bookings WHERE BookingDate = booking_date and TableNumber = table_number;
-    IF bookedTable > 0 THEN
+    IF bookingTable > 0 THEN
       SELECT CONCAT( "Table", table_number, "is already booked") AS "Booking status";
       ELSE 
       SELECT CONCAT( "Table", table_number, "is not booked") AS "Booking status";
--- BookingSystem/UpdateBooking.sql
+++ BookingSystem/UpdateBooking.sql
@@ -1,4 +1,4 @@
-CREATE DEFINER='admin1'@'%' PROCEDURE 'UpdateBooking'(booking_id INT, booking_date DATE)
+CREATE DEFINER='LittleLemonUser1'@'%' PROCEDURE 'UpdateBooking'(booking_id INT, booking_date DATE)
 BEGIN
 UPDATE bookings SET BookingDate = booking_date WHERE BookingID = booking_id; 
 SELECT CONCAT("Booking", booking_id, "updated") AS "Confirmation";
